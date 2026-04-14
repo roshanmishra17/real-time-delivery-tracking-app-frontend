4417a1ea60a812abb32d1bd41db19a38801676a3
akashway <akashmishraway@gmail.com>
2026-04-14 19:42:29 +0530

    Updated the MapSelector for route path between pickup and drop

--- src/pages/MapSelector.jsx
+++ src/pages/MapSelector.jsx
@@ -1,26 +1,42 @@
-import { MapContainer, TileLayer, Marker,useMapEvents} from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, TileLayer, Marker,useMapEvents,Polyline} from "react-leaflet";
+import L from "leaflet"
+import { Popup } from "react-leaflet";
+import { pickupIcon,dropIcon } from "../Component/LiveMap";
 
-function LocationPicker({onSelect}){
+
+function MapClickHandler({ onSelect }) {
     useMapEvents({
-        click(e){
-            onSelect(e.latlng)
+        click(e) {
+            onSelect(e.latlng);
         }
-    })
+    });
+    return null;
 }
 
-export default function MapSelector({onSelect}){
-    const [position,setPosition] = useState(null);
+export default function MapSelector({pickup, drop, route,onSelect}){
 
     return (
         <MapContainer center={[19.076, 72.877]} zoom={13} style={{ height: "300px" }}>
             <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-            <LocationPicker onSelect={(latlng) => {
-                setPosition(latlng);
-                onSelect(latlng);
-            }} />
+            <MapClickHandler onSelect={onSelect} />
 
-            {position && <Marker position={position} />}
+
+            {pickup && (
+                <Marker position={[pickup.lat, pickup.lng]} icon={pickupIcon}>
+                    <Popup>Pickup Location</Popup>
+                </Marker>
+            )}
+            {drop && (
+                <Marker position={[drop.lat, drop.lng]} icon={dropIcon}>
+                    <Popup>Drop Location</Popup>
+                </Marker>
+            )}
+            {route && route.length > 0 && (
+                <Polyline
+                    positions={route.map(p => [p.lat, p.lng])}
+                    pathOptions={{ color: "blue", weight: 5 }}
+                />
+            )}
         </MapContainer>
     )
 }   
