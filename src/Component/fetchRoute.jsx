c7115da8eb4d859dd8c60f7611baccbb6771dd75
akashway <akashmishraway@gmail.com>
2026-04-14 23:09:48 +0530

    Updated the fetchRoute for distance and duration

--- src/Component/fetchRoute.jsx
+++ src/Component/fetchRoute.jsx
@@ -8,16 +8,26 @@ export async function fetchRoute(pickupPoint, dropPoint) {
         const resp = await fetch(url);
         const data = await resp.json();
 
+
         if (data.routes && data.routes.length > 0) {
-            return data.routes[0].geometry.coordinates.map((c) => ({
+            const route = data.routes[0];
+
+            const coords = route.geometry.coordinates.map(c => ({
                 lat: c[1],
-                lng: c[0],
+                lng: c[0]
             }));
+
+            return {
+                path: coords,
+                distance: route.distance,   
+                duration: route.duration
+            };
         }
 
-        return [];
+        return { path: [], distance: 0, duration: 0 };
+
     } catch (err) {
         console.error("Route fetch failed", err);
-        return [];
+        return {path: [], distance: 0, duration: 0};
     }
 }
